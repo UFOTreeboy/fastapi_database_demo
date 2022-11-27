49f09864193d3159be537b49d8e151772b699663
UFOTreeboy <jinn7431080@gmail.com>
2022-11-27 22:11:47 +0800

    2022_11_27_應用程式製作。

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request, Depends, Form, status
+from fastapi.templating import Jinja2Templates
+import models
+from database import engine, sessionlocal
+from sqlalchemy.orm import Session
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+
+models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, Boolean, Text
+from database import Base
+
+#建立資料表
+class Todo(Base):
+    __tablename__ = 'todos'
+    id = Column(Integer, primary_key=True)
+    task = Column(Text)
+    completed = Column(Boolean, default=False)
+
+    def __repr__(self):
+        return '<Todo %r>' % (self.id)
