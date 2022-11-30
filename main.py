ab4e67340c6b5ce7d2b0923e7c917336d23367f1
UFOTreeboy <jinn7431080@gmail.com>
2022-11-30 14:34:55 +0800

    2022_11_30

--- main.py
+++ main.py
@@ -6,10 +6,21 @@ from sqlalchemy.orm import Session
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
+#使用metadata.create_all模組建立位於models的SQL表格
 models.Base.metadata.create_all(bind=engine)
 
+#使用Jinja2Templates模組來讀取templates的網頁檔案
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
+#讀取靜態資源，如圖檔或是CSS
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+#讀取資料庫
+def get_db():
+    db = sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
