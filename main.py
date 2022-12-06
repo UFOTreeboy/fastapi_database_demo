e39e282bfc2af28bf6e3114a6518790422e8a6b7
UFOTreeboy <jinn7431080@gmail.com>
2022-12-06 21:07:14 +0800

    2022_12_6_9

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-import os
-from typing import Optional
-from fastapi import FastAPI
-import uvicorn
-import random
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os , uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"Hello": "World from FastAPI"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# get random number between min(default:0) and max(default:9)
-@app.get("/random/")
-def get_random(min: Optional[int] = 0, max: Optional[int] = 9):
-    rval = random.randint(min, max)
-    return { "value": rval }
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("item.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
