ad991e80ce89f162ffb95b090d53254be86f7526
UFOTreeboy <jinn7431080@gmail.com>
2022-12-09 14:37:37 +0800

    2022_12_9_更改樣式

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request, Depends, Form, status
 from fastapi.templating import Jinja2Templates
+import model
+from database import engine, sessionlocal
+from sqlalchemy.orm import Session
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+
+model.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+def get_db():
+    db = sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-templates = Jinja2Templates(directory="templates")
+@app.get("/")
+async def home(request: Request, db: Session = Depends(get_db)):
+    todos = db.query(model.Todo).order_by(model.Todo.id.desc())
+    return templates.TemplateResponse("index.html", {"request": request, "todos": todos})
 
+@app.post("/add")
+async def add(request: Request, task: str = Form(...),name: str = Form(...) ,db: Session = Depends(get_db)):
+    todo = model.Todo(task=task,name=name)
+    db.add(todo)
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
 
-@app.get("/", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse("item.html", {"request": request})
+@app.get("/edit/{todo_id}")
+async def add(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(model.Todo).filter(model.Todo.id == todo_id).first()
+    return templates.TemplateResponse("edit.html", {"request": request, "todo": todo})
+
+@app.post("/edit/{todo_id}")
+async def add(request: Request, todo_id: int, task: str = Form(...),name: str = Form(...),completed: bool = Form(False), db: Session = Depends(get_db)):
+    todo = db.query(model.Todo).filter(model.Todo.id == todo_id).first()
+    todo.task = task
+    todo.name = name
+    todo.completed = completed
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+
+@app.get("/delete/{todo_id}")
+async def add(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(model.Todo).filter(model.Todo.id == todo_id).first()
+    db.delete(todo)
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
